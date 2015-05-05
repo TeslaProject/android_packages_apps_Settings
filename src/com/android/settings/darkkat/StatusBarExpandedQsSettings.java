228257da55f239bc881c2c595d251a98e9c38c96
John Brewer <gzroms@hotmail.com>
2015-05-04 20:49:26 -0400

    Revert "Update StatusBarExpandedQsSettings.java"
    This reverts commit 05405f77738cb577871399034c6819e8a2382131.

--- src/com/android/settings/darkkat/StatusBarExpandedQsSettings.java
+++ src/com/android/settings/darkkat/StatusBarExpandedQsSettings.java
@@ -20,7 +20,6 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
